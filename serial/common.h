94402df91a89f4b573b7586def6adbf6ce4d012f
fbrizu <frank.brizuela@mail.mcgill.ca>
2014-12-14 17:58:16 -0500

    monicas stuff

--- serial/common.h
+++ serial/common.h
@@ -11,28 +11,11 @@ typedef struct {
 } RGB;
 
 typedef struct {
-	unsigned char R;
-	unsigned char G;
-	unsigned char B;
-} char_RGB;
-
-typedef struct {
-   unsigned int width;
-   unsigned int height;
-   unsigned int planes;
-   unsigned short bitcount;
-   unsigned int size;
-} BITMAPINFOHEADER;
-
-typedef struct {
 	int x;
 	int y;
 } vector;
 
-RGB** allocatePixelMatrix (int x, int y);
-RGB** import_image_from_text(char* img_path);
+void import_image_from_text(RGB** img, char* img_path);
 void export_image_to_text(RGB** img, char* img_path);
-BITMAPINFOHEADER getImageData(FILE *image);
-
 
 #endif
